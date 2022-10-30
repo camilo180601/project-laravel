5df5ef034ab24de2a12bc6722dcea32e07e8e59c
camilo180601 <camilo06180401@gmail.com>
2022-10-30 01:15:53 -0500

    first commit

--- resources/views/peliculas/listado.blade.php
+++ resources/views/peliculas/listado.blade.php
@@ -1,3 +1,45 @@
+@include('includes.header')
+
+<!-- Imprimir por pantalla -->
 <h1>{{$title}}</h1>
 <h2>{{$listado[0]}}</h2>
 <h3>{{ date('Y')}}</h3>
+
+{{-- Comentario Blade --}}
+
+<!--Ver si una variable existe o no -->
+<?= isset($director) ? $director : 'No hay director'; ?>
+<br>
+{{ $director ?? 'No hay director'}}
+<br>
+<!-- condicionales -->
+@if($title && count($listado) >=2 )
+    <h1> titulo existe es este: {{$title}} </h1>
+@else
+    <h1> El titulo no existe </h1>
+@endif
+<!-- Bucles -->
+@for ($i=0; $i<=20; $i++)
+    El numero es: {{$i}}
+    <br>
+@endfor
+<?php
+    $contador = 1
+?>
+<hr>
+@while($contador < 50)
+    @if($contador % 2 == 0)
+        NUMERO PAR: {{$contador}}
+        <br>
+    @endif
+
+    <?php $contador++; ?>
+
+@endwhile
+<hr>
+@foreach($listado as $pelicula)
+    {{$pelicula}} <br>
+@endforeach
+
+
+@include('includes.footer');
